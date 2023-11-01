bf221036b0beaee622067497df0cb0105bc2f4dd
Anand Jha <103054138+divyeanand@users.noreply.github.com>
2023-11-01 22:49:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,10 +23,7 @@ function App() {
 
   const onDelete = (todo) => {
     console.log("I am ondelete of todo", todo);
-    // Deleting this way in react does not work
-    // let index = todos.indexOf(todo);
-    // todos.splice(index, 1);
-
+    
     setTodos(todos.filter((e) => {
       return e !== todo;
     }));
